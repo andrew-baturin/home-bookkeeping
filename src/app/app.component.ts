6d61241384e07f3f302dbef24a8773de37485fef
Andrew Baturin <baturin.dev@gmail.com>
2021-02-23 02:46:58 +0200

    added theme, fonts, and local database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<h1>Working!</h1>'
 })
 export class AppComponent {
-  title = 'home-bookkeeping';
 }
